84307fa6b6787cbd3e27f6e9f907eb9b9695c215
Lisa Halvorsen <kisansimhae@gmail.com>
2021-01-31 11:12:58 +0100

    Error handling and refac - error handle on server side request. So page can be displayed if request failes. - use Stations secions component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,17 @@
 import Head from 'next/head';
-import { StationsList } from '../components/StationsList';
-import { Station, StationResponse } from '../shared/model/Station';
+import { StationResponse } from '../shared/model/Station';
 import { StationStatusResponse } from '../shared/model/StationStatus';
-import { options, getStatuses } from '../shared/fetchHelpers';
-import { useQuery } from 'react-query';
-import { mapStationAndStatus } from '../shared/utils/utils';
-import { format } from 'date-fns';
-import { useEffect, useState } from 'react';
 import styles from '../styles/Home.module.scss';
 import { GetServerSidePropsResult } from 'next';
+import { StationsSection } from '../components/StationsSection';
+import { Nullable } from '../shared/utils/helperTypes';
+import { BYSYKKEL_OPEN_API_URL, options } from '../shared/fetchHelpers';
 
 interface HomeProps {
-  stations: Station[];
-  statuses: StationStatusResponse;
+  stations: Nullable<StationResponse>;
+  statuses: Nullable<StationStatusResponse>;
 }
 export default function Home({ stations, statuses }: HomeProps): JSX.Element {
-  const [lastUpdated, setLastUpdated] = useState('');
-  const { data } = useQuery<StationStatusResponse>('status', getStatuses, {
-    initialData: statuses,
-  });
-  useEffect(() => {
-    if (data && data.last_updated) {
-      setLastUpdated(format(data.last_updated * 1000, 'dd.MM.yyyy HH:mm:ss'));
-    }
-  }, [data, setLastUpdated]);
-
-  const allInfo = mapStationAndStatus(
-    stations,
-    data?.data?.stations ? data?.data?.stations : statuses.data?.stations
-  );
-
   return (
     <div className={styles.container}>
       <Head>
@@ -45,22 +27,12 @@ export default function Home({ stations, statuses }: HomeProps): JSX.Element {
       </p>
       <p>
         Finn ut mer på{' '}
-        <a
-          href="https://oslobysykkel.no/apne-data/sanntid"
-          target="_blank"
-          rel="noreferrer"
-        >
+        <a href={BYSYKKEL_OPEN_API_URL} target="_blank" rel="noreferrer">
           Oslo Bysykkel sine åpne data sider
         </a>
       </p>
-      <p>
-        Sist oppdatert{' '}
-        <time dateTime={new Date(data?.last_updated * 1000).toString()}>
-          {lastUpdated}
-        </time>
-      </p>
 
-      <StationsList stations={allInfo} />
+      <StationsSection stations={stations} statuses={statuses} />
     </div>
   );
 }
@@ -68,15 +40,23 @@ export default function Home({ stations, statuses }: HomeProps): JSX.Element {
 export async function getServerSideProps(): Promise<
   GetServerSidePropsResult<HomeProps>
 > {
-  const stationRes = await fetch(`${process.env.API_STATION}`, options);
-  const stationData: StationResponse = await stationRes.json();
-  const statusRes = await fetch(`${process.env.API_STATION_STATUS}`, options);
-  const statusData: StationStatusResponse = await statusRes.json();
-
-  return {
-    props: {
-      stations: stationData.data.stations,
-      statuses: statusData,
-    },
-  };
+  try {
+    const stationRes = await fetch(`${process.env.API_STATION}`, options);
+    const stationData: StationResponse = await stationRes.json();
+    const statusRes = await fetch(`${process.env.API_STATION_STATUS}`, options);
+    const statusData: StationStatusResponse = await statusRes.json();
+    return {
+      props: {
+        statuses: statusData,
+        stations: stationData,
+      },
+    };
+  } catch (e) {
+    return {
+      props: {
+        statuses: null,
+        stations: null,
+      },
+    };
+  }
 }
