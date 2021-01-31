231e7f68d4f6931a765feee3f76dcd25506db36b
Lisa Halvorsen <kisansimhae@gmail.com>
2021-01-31 11:05:21 +0100

    Refac fetch methods

--- shared/fetchHelpers.ts
+++ shared/fetchHelpers.ts
@@ -1,16 +1,28 @@
 import { StationStatusResponse } from './model/StationStatus';
+import { StationResponse } from './model/Station';
+export const BYSYKKEL_OPEN_API_URL: Readonly<string> =
+  'https://oslobysykkel.no/apne-data/sanntid';
+const API_STATIONS_URL: Readonly<string> =
+  'https://gbfs.urbansharing.com/oslobysykkel.no/station_information.json';
+const API_STATUS_URL: Readonly<string> =
+  'https://gbfs.urbansharing.com/oslobysykkel.no/station_status.json';
 
 export const options = {
   headers: {
     'Client-Identifier': `${process.env.CLIENT_IDENTIFIER}`,
   },
 };
+const fetchGet = async (url: string) => {
+  const res = await fetch(url, options);
+  if (res.ok) {
+    return await res.json();
+  }
+  return Promise.reject(res);
+};
 
 export const getStatuses = async (): Promise<StationStatusResponse> => {
-  const res = await fetch(
-    `https://gbfs.urbansharing.com/oslobysykkel.no/station_status.json`,
-    options
-  );
-  const json: StationStatusResponse = await res.json();
-  return json;
+  return await fetchGet(API_STATUS_URL);
+};
+export const getStations = async (): Promise<StationResponse> => {
+  return await fetchGet(API_STATIONS_URL);
 };
