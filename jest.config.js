af689d08e78a5ae8547d352963b9ec72f432425b
Lisa Halvorsen <kisansimhae@gmail.com>
2021-02-01 19:22:37 +0100

    set up for test

--- jest.config.js
+++ jest.config.js
@@ -7,4 +7,15 @@ module.exports = {
     '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
   },
   transformIgnorePatterns: ['/node_modules/'],
+  moduleNameMapper: {
+    '\\.(scss|css|jpg|png|gif)$': '<rootDir>/tests/file.mock.js',
+  },
 };
+// module.exports = {
+//   testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+//   setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
+//   transform: {
+//     "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
+//     "\\.(css|less|scss|sass)$": "identity-obj-proxy"
+//   }
+// };
