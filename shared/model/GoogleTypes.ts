5c9e52f5232558675727bf265ff128ca6b904042
Lisa Halvorsen <kisansimhae@gmail.com>
2021-02-14 15:39:35 +0100

    Add file for google types

---
+++ shared/model/GoogleTypes.ts
@@ -0,0 +1,4 @@
+export type GoogleMarker = google.maps.Marker;
+export type GoogleLatLng = google.maps.LatLng;
+export type GoogleMap = google.maps.Map;
+export type GoogleMapType = google.maps.MapTypeId;
