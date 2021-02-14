ea58dd0423bf4ef10eb2f237214338725cde1ac5
Lisa Halvorsen <kisansimhae@gmail.com>
2021-02-14 15:38:20 +0100

    Update mocks

---
+++ tests/googleMock.ts
@@ -0,0 +1,13 @@
+window['google'] = {
+  maps: {
+    LatLng: class {},
+    Map: class {},
+    Marker: class {
+      addListener = jest.fn();
+    },
+    InfoWindow: class {},
+    MapTypeId: {
+      ROADMAP: 1,
+    },
+  },
+};
