94a816648606cf0ee5ecd5f3e3ea3d6a3026add8
Lisa Halvorsen <kisansimhae@gmail.com>
2021-02-15 19:13:42 +0100

    Update load map test

--- utils/GoogleMapInit.test.tsx
+++ utils/GoogleMapInit.test.tsx
@@ -1,24 +1,7 @@
 import { loadMap } from './GoogleMapInit';
 
 describe('GoogleMapInit', () => {
-  const OLD_ENV = process.env;
-
-  beforeEach(() => {
-    jest.resetModules(); // Most important - it clears the cache
-    process.env = { ...OLD_ENV }; // Make a copy
-  });
-
-  afterAll(() => {
-    process.env = OLD_ENV; // Restore old environment
-  });
-  test('Creates script', () => {
-    const res = loadMap();
-    expect(res).toBeDefined();
-  });
-
-  test.skip('Throws error when key is not defined', () => {
-    process.env = { NEXT_PUBLIC_GOOGLE_MAPS_KEY: undefined };
-    delete process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY;
+  test('Throws error when key is not defined', () => {
     expect(() => {
       loadMap();
     }).toThrow('No api key defined');
