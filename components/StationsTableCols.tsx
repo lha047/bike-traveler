47be5eb623705f96d44f7cb7a251f1737cc4330a
Lisa Halvorsen <kisansimhae@gmail.com>
2021-02-15 09:00:20 +0100

    Change order of columns
    - to display most important first

--- components/StationsTableCols.tsx
+++ components/StationsTableCols.tsx
@@ -10,9 +10,9 @@ import { Column } from 'react-table';
 export const stationsTableCols = (): Column<AllInfoStation>[] => {
   return [
     { accessor: 'name', Header: HEADING_STATIONS },
-    { accessor: 'address', Header: HEADING_ADRESS },
-    { accessor: 'capacity', Header: HEADING_CAPACITY },
     { accessor: 'num_bikes_available', Header: HEADING_AVAILABLE_BIKES },
     { accessor: 'num_docks_available', Header: HEADING_AVAILABLE_LOCKS },
+    { accessor: 'capacity', Header: HEADING_CAPACITY },
+    { accessor: 'address', Header: HEADING_ADRESS },
   ];
 };
